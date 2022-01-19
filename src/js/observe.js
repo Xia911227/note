5997ee6f0a9e58cae22fb40a7900e0226d7a5ed1
18709290440@163.com <18709290440@163.com>
2022-01-19 10:42:11 +0800

    object observer

---
+++ src/js/observe.js
@@ -0,0 +1,40 @@
+import Dep from './dep'
+
+function defineReactive(data, key, val) {
+    if (typeof val === 'object') { // 递归设置响应式
+        new Observer(val)
+    }
+    let dep = new Dep() // 新增
+    Object.defineProperty(data, key, {
+        enumerable: true,
+        configurable: true,
+        get() {
+            dep.depend()
+            return val
+        },
+        set(newVal) {
+            if (val === newVal) {
+                return
+            }
+            val = newVal
+            dep.notify() // 通知
+        }
+    })
+}
+
+class Observer {
+    constructor(value) {
+        this.value = value
+
+        if (!Array.isArray(value)) {
+            this.walk(value)
+          }
+    }
+
+    walk(obj) {
+        const keys = Object.keys(obj)
+        for (let i = 0; i < keys.length; i++) {
+            defineReactive(obj, keys[i], obj[keys[i]])
+        }
+    }
+}
