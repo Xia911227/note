8c6903f199d1b9cb4a24c72c582476e7bc8760cb
Xia911227 <33925533+Xia911227@users.noreply.github.com>
2023-05-09 15:23:32 +0800

    Create auto.vue

---
+++ auto.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="autofixed" class="autofixed-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Autofixed',
+  methods: {
+    /**
+     * 设置滚动条位置
+     * @name setScrollPosition
+     * @param { DOMRect } rect
+     */
+    setScrollPosition(rect) {
+      /** @type { HTMLDivElement} */
+      const el = this.$refs.autofixed
+      const _rect = el.getBoundingClientRect()
+      el.scrollTop = el.scrollTop + (rect.top - _rect.top)
+    }
+  }
+}
+</script>
